70b335ef8099f4859d2bd51d3e6dfdda7fd037e5
daniel10vieira <danielvieiraalcantara10@gmail.com>
2022-02-21 13:23:17 -0300

    changed some values.

--- JavaScript/PARTE 2/Function argumentos e parâmetros.js
+++ JavaScript/PARTE 2/Function argumentos e parâmetros.js
@@ -6,5 +6,5 @@ function sum(number1, number2){
     console.log(number1 + number2);
 }
 // |ARGUMENTOS 
-sum(2,3) // INVOCA A FUNÇÃO, CHAMA A FUNÇÃO - invoke ro call
+sum(2,3) // INVOCA A FUNÇÃO, CHAMA A FUNÇÃO - invoke or call
 sum(5,9)
